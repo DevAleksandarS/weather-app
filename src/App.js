c4cd61af67fd74306ef057ad02d151b8bc159070
DevAleksandarS <spasojevic.dev@gmail.com>
2022-09-09 15:38:06 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import "./style-css/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="container">
+      <h1 className="logo">WeatherAPP</h1>
+      <div className="flex">
+        <input className="input" type="text" placeholder="City..."></input>
+        <button className="btn-find">Find</button>
+      </div>
+      <p className="text">Current weather in: </p>
+      <p className="city-name">Gradiška</p>
+      <p className="date">15/07/21</p>
     </div>
   );
 }
